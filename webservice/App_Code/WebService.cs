d8a035a66960ec652f559081b47880d91a3ca115
Carlos Sales <carlossalesti@gmail.com>
2017-04-20 19:23:59 -0300

    initial commit

---
+++ webservice/App_Code/WebService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+
+[WebService(Namespace = "http://wservicelogvai.azurewebsites.net")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+    public WebService()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+
+    [WebMethod]
+    public string teste(string v1)
+    {
+        string url = "Aleluia";
+        return url;
+    }
+
+    [WebMethod]
+    public string SalvarUsuario(string param1, string param2, string param3, string param4, string param5,
+        string param6, string param7, string param8, string param9)
+    {
+        string url = "Sorry.aspx";
+        OperacaoBanco operacao = new OperacaoBanco();
+        bool inserir = operacao.Insert("INSERT INTO Tbl_Usuarios (usuario,senha,cpfcnpj,nome,contato,endereco,numero,complemento,telefone,dataCadastro) " +
+            "VALUES ('" + param1 + "', '" + param2 + "', '" + param3 + "', '" + param4 + "', '" + param5 +
+            "', '" + param6 + "', '" + param7 + "', '" + param8 + "', '" + param9 + "', getdate())");
+        ConexaoBancoSQL.fecharConexao();
+        if (inserir == true)
+        {
+            url = "http://logvai.com.br/";
+        }
+        else
+        {
+            url = "Sorry.aspx";
+        }
+
+        return url;
+    }
+
+}
+
+public class ConexaoBancoSQL
+{
+    private static SqlConnection objConexao = null;
+    private string stringconnection1;
+
+    public void tentarAbrirConexaoRemota()
+    {
+        objConexao = new SqlConnection();
+        objConexao.ConnectionString = stringconnection1;
+        objConexao.Open();
+    }
+
+    public ConexaoBancoSQL()
+    {
+        // *** STRING DE CONEXÃO COM BANCO DE DADOS - Atenção! Alterar dados conforme seu servidor
+        stringconnection1 = @"Server=tcp:serverlogvai.database.windows.net,1433;Initial Catalog=dblogvai;Persist Security Info=False;User ID=admserver;Password=pwd@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        try
+        {
+            tentarAbrirConexaoRemota();
+        }
+        catch
+        {
+            Console.WriteLine("Atenção! Não foi possível Conectar ao Servidor de Banco de Dados.");
+        }
+    }
+
+    public static SqlConnection getConexao()
+    {
+        new ConexaoBancoSQL();
+        return objConexao;
+    }
+    public static void fecharConexao()
+    {
+        objConexao.Close();
+    }
+}
+
+public class OperacaoBanco
+{
+    private SqlCommand TemplateMethod(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return Commando;
+        }
+        catch
+        {
+            return Commando;
+        }
+    }
+
+    public SqlDataReader Select(String query)
+    {
+        SqlDataReader dadosObtidos = TemplateMethod(query).ExecuteReader();
+        return dadosObtidos;
+    }
+
+    public Boolean Insert(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Update(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Delete(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+}
